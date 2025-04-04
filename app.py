0140bc40ba6f21e3ff4754ddb7202dd9bcbf65b2
hansli112 <hansli112@yahoo.com.tw>
2025-04-04 12:42:16 +0800

    feat: add payment

--- app.py
+++ app.py
@@ -23,14 +23,16 @@ item, team = re.split("（|）", item_name)[:2]
 item_id = item_list.index[(item_list["名稱"] == item) & (item_list["短宣隊"] == team)].item()
 
 # show details
-st.text(f"名稱：{item_list.loc[item_id, '名稱']}")
 st.text(f"金額：${item_list.loc[item_id, '單價']}/份")
 st.text(f"剩餘：{item_list.loc[item_id, '剩餘數量']}份")
-st.text(item_list.loc[item_id, "短宣隊"])
 
 st.divider()
 
 # input donation
+payment = st.selectbox("奉獻方式", ["現金", "銀行轉帳"])
+if payment == "銀行轉帳":
+    st.caption("請轉至：國泰世華 (013) 127506314089")
+
 number = st.number_input("數量", 0, item_list.loc[item_id, "剩餘數量"])
 total_money = item_list.loc[item_id, "單價"] * number
 st.text(f"總金額：${total_money}")
@@ -43,7 +45,7 @@ if st.button("送出", disabled=st.session_state["is_submit"]):
         if not st.session_state["is_submit"]:
             # save record
             record = pd.DataFrame(
-                data={"姓名": [name], "物資": [item], "數量": [number], "總金額": [total_money], "短宣隊": [team]}
+                    data={"姓名": [name], "物資": [item], "數量": [number], "總金額": [total_money], "短宣隊": [team], "奉獻方式": [payment]}
             )
             out_path = "record.csv"
             record.to_csv(out_path, mode='a', header=not os.path.exists(out_path), index=False)
